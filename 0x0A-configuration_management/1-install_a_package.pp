8ea4b04b2eba74dd81a857b98f0d3ebe91659ae4
lensonlovescode <lensonisinparis@gmail.com>
2025-01-25 08:54:09 +0300

    corrections

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-#A puppet script that installs version flask v2.1.0 on a node
-package { 'flask' :
+# A puppet script that installs version flask v2.1.0 on a node
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
