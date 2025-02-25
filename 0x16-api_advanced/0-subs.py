deca7814c7d7d64b0a0e33b21cacab8422325674
lensonlovescode <lensonisinparis@gmail.com>
2025-02-26 00:23:14 +0300

    Corrections task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,7 +16,7 @@ def number_of_subscribers(subreddit):
     url = "https://reddit.com/r/" + subreddit + "/about.json"
     headers = {"User-Agent": "my-bot/0.1"}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         return (0)
