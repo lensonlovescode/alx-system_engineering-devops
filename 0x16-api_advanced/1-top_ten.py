07f8a4b1270226c75ddd15551c479ee1fe6033d8
lensonlovescode <lensonisinparis@gmail.com>
2025-02-26 00:19:33 +0300

    Top 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,9 +12,9 @@ def top_ten(subreddit):
     A function that queries the Reddit API and prints the
     titles of the first 10 hot posts listed for a given subreddit.
     """
-    url  = "https://www.reddit.com/r/" + subreddit + "/hot.json?limit=9"
+    url = "https://www.reddit.com/r/" + subreddit + "/hot.json?limit=9"
     headers = {"User-Agent": "my-bot/0.1"}
-    
+
     request = requests.get(url, headers=headers, allow_redirects=False)
 
     if request.status_code != 200:
